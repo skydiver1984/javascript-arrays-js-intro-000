bc438cff3b71965786e2cbcaecc2172832e292fd
Luis Hawkins <bladerunnerluis@gmail.com>
2020-03-13 00:45:57 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -7,7 +7,7 @@ function addElementToBeginningOfArray(array, element) {
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
-  return array; 
+  return array;
 }
 
 function addElementToEndOfArray(array, element) {
